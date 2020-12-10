ce6479e13624da54a746980159efaf8ba30e3956
Stanley Masinde <stanmasinde@gmail.com>
2020-12-10 21:58:10 +0300

    Removed the unnecessary fetch hook

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,3 @@
   </h1>
 </template>
 <script>
-export default {
-  async fetch () {
-    try {
-      await this.$axiod.get
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-}
-</script>

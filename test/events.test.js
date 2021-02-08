40e68c0f91d42fdf22d5e50da3c08e0c26f1146a
Stanley Masinde <stanmasinde@gmail.com>
2021-02-08 21:22:29 +0300

    Updated the tests read below for changes  - The tests were using an unnecessary dependency supertest  - Improved the tests to be using async await  - All the requests are authenticated as intended without including an unnecessary dependency  - The tests are easy to read

--- test/events.test.js
+++ test/events.test.js
@@ -1,36 +1,26 @@
+/* eslint-disable no-undef */
 const fs = require('fs')
 const chai = require('chai')
 const { expect } = require('chai')
 const chaiHttp = require('chai-http')
 const faker = require('faker')
 chai.use(chaiHttp)
-
-const request = require('supertest')
-
 const application = require('../api/app')
+const app = chai.request.agent(application).keepOpen() // This will keep the request open and so the session
 
-let cookie
 describe('#Events test with protected routes', () => {
-  // using auth methods
-  // eslint-disable-next-line no-undef
-  before((done) => {
-    request(application)
+  // Login a user to perform these requests. All requests in this describe block will share cookies
+  before(async () => {
+    await app
       .post('/auth/login')
       .send({
         email: 'john@example.com',
         password: '12345678'
       })
-      .expect(200)
-      .end(function (err, res) {
-        if (err) { return done(err) }
-        cookie = res.headers['set-cookie']
-        return done()
-      })
   })
 
-  it('User creates and event', (done) => {
-    request(application).post('/events')
-      .set('cookie', cookie)
+  it('User creates and event', async () => {
+    const res = await app.post('/events')
       .set('content-type', 'multipart/form-data')
       .attach('poster', fs.readFileSync('./static/icon.png'), 'icon.png')
       .field({
@@ -40,27 +30,19 @@ describe('#Events test with protected routes', () => {
         from_date: new Date().toISOString().substr(0, 10),
         from_time: '09:30'
       })
-      .expect(201)
-      .end(function (err, res) {
-        if (err) { return done(err) }
-        return done()
-      })
+
+    expect(res.status).equals(201)
   })
 
-  it('Get current user\'s events', (done) => {
-    request(application).get('/events/currentUser')
-      .set('cookie', cookie)
-      .set('Accept', 'application/json')
-      .expect(200)
-      .end(function (err, res) {
-        if (err) { return done(err) }
-        return done()
-      })
+  it('Get current user\'s events', async () => {
+    const res = await app.get('/events/currentUser')
+    expect(res.status).equals(200)
+    expect(res.body).to.be.an('Array')
   })
 
-  it('Update an event', (done) => {
-    request(application).put('/events/1')
-      .set('cookie', cookie)
+  it('Update an event', async () => {
+    const res = await app
+      .put('/events/1')
       .send({
         type: 'Online',
         title: 'New title',
@@ -68,31 +50,17 @@ describe('#Events test with protected routes', () => {
         from_date: new Date().toISOString().substr(0, 10),
         from_time: '10:45'
       })
-      .expect(201)
-      .end(function (err, res) {
-        if (err) { return done(err) }
-        return done()
-      })
+
+    expect(res.status).equals(201)
   })
 
-  it('Get the updated Event', (done) => {
-    request(application).get('/events/1')
-      .set('cookie', cookie)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body.title).equals('New title')
-        if (err) { return done(err) }
-        return done()
-      })
+  it('Get the updated Event', async () => {
+    const res = await app.get('/events/1')
+    expect(res.body.title).equals('New title')
   })
 
-  it('User deletes and event', (done) => {
-    request(application).delete('/events/1')
-      .set('cookie', cookie)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) { return done(err) }
-        return done()
-      })
+  it('User deletes and event', async () => {
+    const res = await app.delete('/events/1')
+    expect(res.body).equals('Deleted')
   })
 })

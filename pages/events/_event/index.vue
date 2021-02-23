bea07cefeae1d087af63cc346b74fbab9d9fbb73
Stanley Masinde <stanmasinde@gmail.com>
2021-02-23 13:19:45 +0300

    Users can now register for events from the client side
    // This feature is just a place holder. Further improvements will be made to the tickets and RSVP tables
    // This will include the emails

--- pages/events/_event/index.vue
+++ pages/events/_event/index.vue
@@ -3,7 +3,11 @@
     <v-row justify="center">
       <v-col cols="12" md="8">
         <v-card class="mx-auto" flat>
-          <v-img :src="currentEvent.poster_url" class="align-end" height="350px">
+          <v-img
+            :src="currentEvent.poster_url"
+            class="align-end"
+            height="350px"
+          >
             <v-card-title>
               <h1 class="custom-shadow white--text">
                 {{ currentEvent.title }}
@@ -14,7 +18,7 @@
                 <v-icon class="white--text">
                   mdi-map-marker
                 </v-icon>
-                {{ currentEvent.venue || 'To be decided' }}
+                {{ currentEvent.venue || "To be decided" }}
               </h3>
               <h3 class="custom-shadow white--text">
                 <v-icon class="white--text">
@@ -25,13 +29,22 @@
             </v-card-subtitle>
           </v-img>
           <v-card-actions>
-            <v-btn v-if="currentEvent.can_edit" text :to="`/events/${currentEvent.id}/manage`">
+            <v-btn
+              v-if="currentEvent.can_edit"
+              text
+              :to="`/events/${currentEvent.id}/manage`"
+            >
               Manage event
             </v-btn>
             <v-btn icon color="primary">
               <v-icon>mdi-share</v-icon>
             </v-btn>
-            <v-btn v-if="!currentEvent.can_edit" depressed color="primary" @click="registerDialog = true">
+            <v-btn
+              v-if="!currentEvent.can_edit"
+              depressed
+              color="primary"
+              @click="showRegistrationDialog"
+            >
               Register for this event
             </v-btn>
           </v-card-actions>
@@ -47,7 +60,12 @@
         <v-card-title>Register for event</v-card-title>
         <v-card-text>
           <v-form>
-            <v-select v-model="eventRsvp.ticket_id" label="Select ticket" outlined :items="['VIP', 'Regular', 'General']" />
+            <v-select
+              v-model="eventRsvp.ticket"
+              label="Select ticket"
+              outlined
+              :items="ticks"
+            />
             <v-text-field
               v-model="eventRsvp.rsvp_count"
               readonly
@@ -58,10 +76,17 @@
               label="How many tickets?"
               outlined
               @click:append-outer="eventRsvp.rsvp_count++"
-              @click:prepend="eventRsvp.rsvp_count > 1 ? eventRsvp.rsvp_count-- : ''"
+              @click:prepend="
+                eventRsvp.rsvp_count > 1 ? eventRsvp.rsvp_count-- : ''
+              "
             />
-            Order total: KES 0.00
-            <v-btn color="primary" block>
+            Order total:
+            {{
+              formatCurrency(
+                eventRsvp.ticket.price | (0 * eventRsvp.rsvp_count)
+              )
+            }}
+            <v-btn color="primary" block @click="confirmOrder">
               Confirm order
             </v-btn>
           </v-form>
@@ -74,8 +99,9 @@
 export default {
   data () {
     return {
+      availableTickets: [],
       eventRsvp: {
-        ticket_id: null,
+        ticket: {},
         rsvp_count: 1
       },
       currentEvent: {},
@@ -84,16 +110,58 @@ export default {
   },
   async fetch () {
     try {
-      const { data } = await this.$axios.get(`/api/events/${this.$route.params.event}`)
+      const { data } = await this.$axios.get(
+        `/api/events/${this.$route.params.event}`
+      )
       this.currentEvent = data
     } catch (error) {
       throw new Error(error)
+    }
+  },
+  computed: {
+    ticks () {
+      return this.availableTickets.map((t) => {
+        return {
+          text: `${t.description} - ${this.formatCurrency(t.price)}`,
+          value: t
+        }
+      })
+    }
+  },
+  methods: {
+    async confirmOrder () {
+      try {
+        await this.$axios.post(
+          `/api/events/${this.$route.params.event}/register`,
+          {
+            ticket_id: this.eventRsvp.ticket.id,
+            rsvp_count: this.eventRsvp.rsvp_count
+          }
+        )
+        // TODO I honestly don't know what to do here
+        // I will just close the Modal for now
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    formatCurrency (value = 0) {
+      return Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'kes'
+      }).format(value)
+    },
+    async showRegistrationDialog () {
+      this.registerDialog = true
+      const { data } = await this.$axios.get(
+        `/api/events/${this.$route.params.event}/tickets`
+      )
+      this.availableTickets = data
     }
   }
 }
 </script>
 <style lang="scss">
 .custom-shadow {
-    text-shadow: 3px 3px 6px black;
+  text-shadow: 3px 3px 6px black;
 }
 </style>

86bc16313da8350670f2c87574573624a86177b5
Stanley Masinde <stanmasinde@gmail.com>
2021-02-26 18:46:59 +0300

    Updated the event seeders

--- seeds/create_events.js
+++ seeds/create_events.js
@@ -1,20 +1,31 @@
 const faker = require('faker')
 exports.seed = function (knex) {
-  const rows = []
+  const events = []
+  const tickets = []
 
   for (let row = 0; row < 50; row++) {
-    rows.push({
+    // Create events
+    events.push({
       image: faker.image.abstract(),
       location: faker.address.streetAddress(),
       online_link: faker.internet.url(),
       about: faker.lorem.words(),
-      description: faker.unique(),
+      description: faker.lorem.paragraphs(5),
       startDate: faker.date.future(),
       endDate: faker.date.future(),
       organisable_id: 10,
       organisable_type: 'User'
     })
+    // Create tickets
+    tickets.push({
+      event_id: row + 1,
+      price: faker.commerce.price(100),
+      limit: 0,
+      type: 'General admission'
+    })
   }
   // Deletes ALL existing entries
-  return knex('events').insert(rows)
+  return knex('events').insert(events).then(() => {
+    knex('tickets').insert(tickets)
+  })
 }

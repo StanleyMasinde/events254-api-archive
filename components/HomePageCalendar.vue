c1d08114b6f607a3de7f3bb7d6b1060abf422c71
Stanley Masinde <stanmasinde@gmail.com>
2021-03-11 15:29:43 +0300

    Landing page mockup

---
+++ components/HomePageCalendar.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="calendar-container">
+    <div class="date-container">
+      <div class="date">
+        {{ today.date() }}
+      </div>
+      <div class="day">
+        {{ days[today.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusOne.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusOne.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusTwo.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusTwo.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusThree.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusThree.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusFour.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusFour.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusFive.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusFive.day()] }}
+      </div>
+    </div>
+
+    <div class="date-container">
+      <div class="date">
+        {{ todayPlusSix.date() }}
+      </div>
+      <div class="day">
+        {{ days[todayPlusSix.day()] }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    }
+  },
+  computed: {
+    today () {
+      return this.$moment()
+    },
+    todayPlusOne () {
+      return this.$moment().add(1, 'day')
+    },
+    todayPlusTwo () {
+      return this.$moment().add(2, 'days')
+    },
+    todayPlusThree () {
+      return this.$moment().add(3, 'days')
+    },
+    todayPlusFour () {
+      return this.$moment().add(4, 'days')
+    },
+    todayPlusFive () {
+      return this.$moment().add(5, 'days')
+    },
+    todayPlusSix () {
+      return this.$moment().add(6, 'days')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+div {
+  .calendar-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    overflow-x: scroll;
+  }
+
+  &.date-container {
+    width: 100vw;
+    text-align: center;
+    cursor: pointer;
+
+    &:hover {
+        color: #ffffff;
+        background-color: teal;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,81 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" md="8">
-      <h1 class="display-1">
-        Discover upcoming events
-      </h1>
-    </v-col>
+  <div>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <h1 class="display-1">
+          Discover upcoming events
+        </h1>
+      </v-col>
+    </v-row>
 
-    <v-col cols="12" md="8">
-      <v-text-field outlined rounded placeholder="Find something awesome" append-icon="mdi-magnify" />
-    </v-col>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-text-field
+          outlined
+          rounded
+          placeholder="Find something awesome"
+          append-icon="mdi-magnify"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <HomePageCalendar />
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <h3 class="title">
+          Browse by category
+        </h3>
+      </v-col>
+      <v-col cols="12" md="8">
+        <v-chip-group>
+          <v-chip>Technology</v-chip>
+          <v-chip>Sports</v-chip>
+          <v-chip>Religious</v-chip>
+          <v-chip>AI</v-chip>
+          <v-chip>Cars</v-chip>
+          <v-chip>Better living</v-chip>
+          <v-chip>Webinars</v-chip>
+          <v-chip>Hackathons</v-chip>
+        </v-chip-group>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" md="6">
+        <h3>Events near Nairobi</h3>
+
+        <v-card
+          v-for="(e, i) in events"
+          :key="i"
+          outlined
+          :to="`/events/${e.id}`"
+          class="ma-2"
+          rounded
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ e.about }}</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-icon>mdi-calendar</v-icon>
+                {{ new Date(e.startDate).toDateString() }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                <v-icon>mdi-map-marker</v-icon>
+                {{ e.location }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <v-img height="100" width="100" :src="e.image" />
+            </v-list-item-icon>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <v-col cols="12">
       <v-btn
@@ -22,10 +89,23 @@
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-col>
-  </v-row>
+  </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      events: []
+    }
+  },
+  async fetch () {
+    try {
+      const { data } = await this.$axios.get('/api/p/events')
+      this.events = data
+    } catch (error) {
+      throw new Error(error)
+    }
+  },
   auth: false
 }
 </script>

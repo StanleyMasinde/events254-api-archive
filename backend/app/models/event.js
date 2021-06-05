7dbae3955cbf7ab4fe0f4c98a47e12fbc4f7be41
Stanley Masinde <stanmasinde@gmail.com>
2021-06-05 16:08:55 +0300

    Fallback for missing dates in the landing page  * Exprermental loading of tickets from the ticket table

--- backend/app/models/event.js
+++ backend/app/models/event.js
@@ -24,10 +24,29 @@ class Event extends Model {
     const lastPage = parseInt(records[0].recordCount / paginate)
     const events = await DB(this.tableName())
       .where('startDate', '>', today)
+      .join('tickets', 'tickets.event_id', '=', 'events.id')
       .limit(paginate)
       .offset(offset)
       .orderBy('startDate', 'asc')
       .select()
+
+    events.forEach((event) => {
+      if (!event.location) {
+        event.isOnline = true
+      } else {
+        event.isOnline = false
+      }
+      if (!event.endDate) {
+        event.endDate = event.startDate
+      }
+      for (const key in event) {
+        if (Object.hasOwnProperty.call(event, key)) {
+          if (event[key] == null) {
+            event[key] = 'N/A'
+          }
+        }
+      }
+    })
     return {
       events, lastPage, remaining
     }

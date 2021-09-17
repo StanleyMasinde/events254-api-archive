4ea75ac3d789a7328da3dbe3ca02875dc0220208
Stanley Masinde <stanmasinde@gmail.com>
2021-09-17 11:55:56 +0300

    chore(deployment): updated the deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
-echo "Reseting local changes"
-git reset --hard
-
-echo "Starting the deployment process"
-git pull
-
 echo "Installing dependencies with npm"
 npm i
 
 echo "Running the latest migrations"
 npx knex migrate:latest --env production
 
-# echo "Building for production"
-# npm run build -- -q
-
 echo "Restarting the process"
-#pm2 restart all
+pm2 restart ecosystem.config.js

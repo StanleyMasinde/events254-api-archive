0da469350aba73e2b2bfd01298e2ad4a67b96a5f
Stanley Masinde <stanmasinde@gmail.com>
2021-10-11 23:14:16 +0300

    chore(deploymnet): added ignore watch directories

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
     args: '-- run start',
     watch: false,
     exec_mode: 'cluster',
-    instances: 1
+    instances: 1,
+    ignore_watch: ['node_modules', 'logs', 'public', 'uploads'],
   }
   ]
 }

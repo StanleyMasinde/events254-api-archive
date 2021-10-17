6e44e3ca1fe3c152c79cdeed6496c64ef92fa69e
Stanley Masinde <stanmasinde@gmail.com>
2021-10-17 20:17:03 +0300

    feat(landing): added next month events filter

--- app/models/event.js
+++ app/models/event.js
@@ -21,6 +21,9 @@ class Event extends Model {
 		const now = moment.tz('Africa/Nairobi').format('YYYY-MM-DD HH:mm:ss')
 		const endOfWeek = moment.tz('Africa/Nairobi').endOf('week').format('YYYY-MM-DD HH:mm:ss')
 		const endOfMonth = moment.tz('Africa/Nairobi').endOf('month').format('YYYY-MM-DD HH:mm:ss')
+		const startOfNextMonth = moment.tz('Africa/Nairobi').add(1, 'month').startOf('month').format('YYYY-MM-DD HH:mm:ss')
+		const endOfNextMonth = moment.tz('Africa/Nairobi').add(1, 'month').endOf('month').format('YYYY-MM-DD HH:mm:ss')
+		const nextMonth = moment.tz('Africa/Nairobi').add(1, 'month').format('MMMM')
 		try {
 			const upcomingEvents = await DB('events')
 				.where('startDate', '>=', now)
@@ -47,12 +50,19 @@ class Event extends Model {
 				.whereRaw(`online_link IS NOT NULL AND startDate >= '${now}'`)
 				.orderBy('startDate', 'asc')
 				.limit(paginate)
+			const nextMonthEvents = await DB('events')
+				.whereRaw(`startDate BETWEEN '${startOfNextMonth}' AND '${endOfNextMonth}'`)
+				.orderBy('startDate', 'asc')
+				.limit(paginate)
+				.select()
+			
 	
 			
 			// Select from tickets where event_id = each event id
 			const upcomingEventsTickets = await DB('tickets').select('*').where('event_id', 'IN', upcomingEvents.map(e => e.id))
 			const happeningThisWeekTickets = await DB('tickets').select('*').where('event_id', 'IN', happeningThisWeek.map(e => e.id))
 			const happeningThisMonthTickets = await DB('tickets').select('*').where('event_id', 'IN', happeningThisMonth.map(e => e.id))
+			const nextMonthEventsTickets = await DB('tickets').select('*').where('event_id', 'IN', nextMonthEvents.map(e => e.id))
 
 
 			upcomingEvents.forEach((event) => {
@@ -217,6 +227,49 @@ class Event extends Model {
 				}
 			})
 
+			nextMonthEvents.forEach((event) => {
+				const ticketsForEvent = nextMonthEventsTickets.filter(ticket => ticket.event_id === event.id)
+				event.soldOut = ticketsForEvent.length === 0
+
+				if (event.soldOut) {
+					event.lowestPrice = 0
+					event.highestPrice = 0
+				} else {
+					event.lowestPrice = ticketsForEvent.reduce((prev, current) => {
+						if (prev.price > current.price) {
+							return current
+						}
+						return prev
+					}).price
+					event.highestPrice = ticketsForEvent.reduce((prev, current) => {
+						if (prev.price < current.price) {
+							return current
+						}
+						return prev
+					}).price
+				}
+				event.isFree = event.lowestPrice === 0
+				if (!event.location) {
+					event.isOnline = true
+				} else {
+					event.isOnline = false
+				}
+				if (!event.endDate) {
+					event.isAllDay = true
+					event.endDate = event.startDate
+				}
+				for (const key in event) {
+					if (Object.hasOwnProperty.call(event, key)) {
+						if (event[key] == null) {
+							event[key] = 'N/A'
+						}
+					}
+				}
+			})
+
+
+				
+
 
 			return {
 				upcomingEvents: {
@@ -230,6 +283,10 @@ class Event extends Model {
 				happeningThisMonth: {
 					name: 'This month',
 					events: happeningThisMonth
+				},
+				nextMonthEvents: {
+					name: nextMonth,
+					events: nextMonthEvents
 				},
 				freeEvents: {
 					name: 'Free events',
